4dce34647a30721f8eec6eb021f49211e7225222
Alexsandro Oliveira <alexsandrolt1996@gmail.com>
2025-11-26 17:41:33 -0300

    fix: add vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",        // 👈 ESSENCIAL para Vercel carregar imagens corretamente
   plugins: [react(), tailwindcss()],
 })
